69b9440e3a7e013990908ca9da1da8362f9827d1
Francesco Vergona <vergona@amazon.com>
2022-06-17 10:18:58 +0200

    Changed lambda content, now having only the specific function's code. Added layer for the ProductStore to avoid code replication

--- load-testing/run-load-test.sh
+++ load-testing/run-load-test.sh
@@ -4,7 +4,7 @@
 STACK_NAME=serverless-python-demo
 
 API_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME \
-  --query 'Stacks[0].Outputs[?OutputKey==`ApiURL`].OutputValue' \
+  --query 'Stacks[0].Outputs[?OutputKey==`ServerlessDemoApi`].OutputValue' \
   --output text)
 
 echo $API_URL
